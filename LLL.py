7942e1e6efe5571370075e4a256506191ee53592
Philip Wang <powdercheeze@gmail.com>
2019-09-10 11:57:24 -0400

    Update LLL.py (unoptimized)

--- LLL.py
+++ LLL.py
@@ -4,6 +4,9 @@ from copy import deepcopy
 
 d = 0.25
 
+def mu(b_i, b_j):
+	return b_i.dot_product(b_j) / b_j.dot_product(b_j)
+
 def LLL(l_basis):
 	ortho = gram_schmidt(l_basis)
 
@@ -13,4 +16,4 @@ def LLL(l_basis):
 
 	while k <= n:
 		for j in range(k - 1, 0, -1):
-			
+			b_j, b_k = ortho[j], ortho[k]
